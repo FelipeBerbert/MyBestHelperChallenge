ccf353fb496a43678a29fe0583cab36a4a74316e
angeloorrico <a.orrico@gmail.com>
2016-05-21 13:18:50 -0300

    WelcomeActivity

---
+++ app/src/main/java/vanhackathon2016/mybesthelperchallenge/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package vanhackathon2016.mybesthelperchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vanhackathon2016.mybesthelperchallange.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
