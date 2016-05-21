afa9cdd56ef6940be1319c3b003fae6d4bea5b29
angeloorrico <a.orrico@gmail.com>
2016-05-21 14:51:43 -0300

    Welcome class moved from package and integrated with Annotations.

---
+++ app/src/main/java/vanhackathon2016/mybesthelperchallenge/gui/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package vanhackathon2016.mybesthelperchallenge.gui;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import vanhackathon2016.mybesthelperchallange.R;
+
+@EActivity(R.layout.activity_welcome)
+public class WelcomeActivity extends AppCompatActivity {
+
+
+}
