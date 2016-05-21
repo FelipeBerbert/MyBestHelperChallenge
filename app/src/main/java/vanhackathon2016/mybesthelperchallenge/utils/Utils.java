b5d186e7f2feeba4439ffe8701483a32b7d76603
angeloorrico <a.orrico@gmail.com>
2016-05-21 18:34:57 -0300

    Definition os questions and answers.

--- app/src/main/java/vanhackathon2016/mybesthelperchallenge/utils/Utils.java
+++ app/src/main/java/vanhackathon2016/mybesthelperchallenge/utils/Utils.java
@@ -6,75 +6,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 import vanhackathon2016.mybesthelperchallange.R;
+import vanhackathon2016.mybesthelperchallenge.models.AnswerModel;
 
 /**
  * Created by Angelo on 21/05/2016.
  */
 public class Utils {
 
-    public List<String> getAnswersToQuestion(Context c, int questionId) {
-        ArrayList<String> answers = new ArrayList<>();
+    public List<AnswerModel> getAnswersToQuestion(Context c, int questionId) {
+        ArrayList<AnswerModel> answers = new ArrayList<>();
 
         switch (questionId) {
             case 1:
-                answers.add(c.getResources().getString(R.string.answer_1_question_1));
-                answers.add(c.getResources().getString(R.string.answer_1_question_2));
-                answers.add(c.getResources().getString(R.string.answer_1_question_3));
-                answers.add(c.getResources().getString(R.string.answer_1_question_4));
+                answers.add(new AnswerModel(1, c.getResources().getString(R.string.answer_1_question_1)));
+                answers.add(new AnswerModel(1, c.getResources().getString(R.string.answer_1_question_2)));
+                answers.add(new AnswerModel(1, c.getResources().getString(R.string.answer_1_question_3)));
+                answers.add(new AnswerModel(1, c.getResources().getString(R.string.answer_1_question_4)));
                 break;
             case 2:
-                answers.add(c.getResources().getString(R.string.answer_2_question_1));
-                answers.add(c.getResources().getString(R.string.answer_2_question_2));
-                answers.add(c.getResources().getString(R.string.answer_2_question_3));
-                answers.add(c.getResources().getString(R.string.answer_2_question_4));
+                answers.add(new AnswerModel(2, c.getResources().getString(R.string.answer_2_question_1)));
+                answers.add(new AnswerModel(2, c.getResources().getString(R.string.answer_2_question_2)));
+                answers.add(new AnswerModel(2, c.getResources().getString(R.string.answer_2_question_3)));
+                answers.add(new AnswerModel(2, c.getResources().getString(R.string.answer_2_question_4)));
                 break;
             case 3:
-                answers.add(c.getResources().getString(R.string.answer_3_question_1));
-                answers.add(c.getResources().getString(R.string.answer_3_question_2));
-                answers.add(c.getResources().getString(R.string.answer_3_question_3));
-                answers.add(c.getResources().getString(R.string.answer_3_question_4));
+                answers.add(new AnswerModel(3, c.getResources().getString(R.string.answer_3_question_1)));
+                answers.add(new AnswerModel(3, c.getResources().getString(R.string.answer_3_question_2)));
+                answers.add(new AnswerModel(3, c.getResources().getString(R.string.answer_3_question_3)));
+                answers.add(new AnswerModel(3, c.getResources().getString(R.string.answer_3_question_4)));
                 break;
             case 4:
-                answers.add(c.getResources().getString(R.string.answer_4_question_1));
-                answers.add(c.getResources().getString(R.string.answer_4_question_2));
-                answers.add(c.getResources().getString(R.string.answer_4_question_3));
-                answers.add(c.getResources().getString(R.string.answer_4_question_4));
+                answers.add(new AnswerModel(4, c.getResources().getString(R.string.answer_4_question_1)));
+                answers.add(new AnswerModel(4, c.getResources().getString(R.string.answer_4_question_2)));
+                answers.add(new AnswerModel(4, c.getResources().getString(R.string.answer_4_question_3)));
+                answers.add(new AnswerModel(4, c.getResources().getString(R.string.answer_4_question_4)));
                 break;
             case 5:
-                answers.add(c.getResources().getString(R.string.answer_5_question_1));
-                answers.add(c.getResources().getString(R.string.answer_5_question_2));
-                answers.add(c.getResources().getString(R.string.answer_5_question_3));
-                answers.add(c.getResources().getString(R.string.answer_5_question_4));
+                answers.add(new AnswerModel(5, c.getResources().getString(R.string.answer_5_question_1)));
+                answers.add(new AnswerModel(5, c.getResources().getString(R.string.answer_5_question_2)));
+                answers.add(new AnswerModel(5, c.getResources().getString(R.string.answer_5_question_3)));
+                answers.add(new AnswerModel(5, c.getResources().getString(R.string.answer_5_question_4)));
                 break;
             case 6:
-                answers.add(c.getResources().getString(R.string.answer_6_question_1));
-                answers.add(c.getResources().getString(R.string.answer_6_question_2));
-                answers.add(c.getResources().getString(R.string.answer_6_question_3));
-                answers.add(c.getResources().getString(R.string.answer_6_question_4));
+                answers.add(new AnswerModel(6, c.getResources().getString(R.string.answer_6_question_1)));
+                answers.add(new AnswerModel(6, c.getResources().getString(R.string.answer_6_question_2)));
+                answers.add(new AnswerModel(6, c.getResources().getString(R.string.answer_6_question_3)));
+                answers.add(new AnswerModel(6, c.getResources().getString(R.string.answer_6_question_4)));
                 break;
             case 7:
-                answers.add(c.getResources().getString(R.string.answer_7_question_1));
-                answers.add(c.getResources().getString(R.string.answer_7_question_2));
-                answers.add(c.getResources().getString(R.string.answer_7_question_3));
-                answers.add(c.getResources().getString(R.string.answer_7_question_4));
+                answers.add(new AnswerModel(7, c.getResources().getString(R.string.answer_7_question_1)));
+                answers.add(new AnswerModel(7, c.getResources().getString(R.string.answer_7_question_2)));
+                answers.add(new AnswerModel(7, c.getResources().getString(R.string.answer_7_question_3)));
+                answers.add(new AnswerModel(7, c.getResources().getString(R.string.answer_7_question_4)));
                 break;
             case 8:
-                answers.add(c.getResources().getString(R.string.answer_8_question_1));
-                answers.add(c.getResources().getString(R.string.answer_8_question_2));
-                answers.add(c.getResources().getString(R.string.answer_8_question_3));
-                answers.add(c.getResources().getString(R.string.answer_8_question_4));
+                answers.add(new AnswerModel(8, c.getResources().getString(R.string.answer_8_question_1)));
+                answers.add(new AnswerModel(8, c.getResources().getString(R.string.answer_8_question_2)));
+                answers.add(new AnswerModel(8, c.getResources().getString(R.string.answer_8_question_3)));
+                answers.add(new AnswerModel(8, c.getResources().getString(R.string.answer_8_question_4)));
                 break;
             case 9:
-                answers.add(c.getResources().getString(R.string.answer_9_question_1));
-                answers.add(c.getResources().getString(R.string.answer_9_question_2));
-                answers.add(c.getResources().getString(R.string.answer_9_question_3));
-                answers.add(c.getResources().getString(R.string.answer_9_question_4));
+                answers.add(new AnswerModel(9, c.getResources().getString(R.string.answer_9_question_1)));
+                answers.add(new AnswerModel(9, c.getResources().getString(R.string.answer_9_question_2)));
+                answers.add(new AnswerModel(9, c.getResources().getString(R.string.answer_9_question_3)));
+                answers.add(new AnswerModel(9, c.getResources().getString(R.string.answer_9_question_4)));
                 break;
             case 10:
-                answers.add(c.getResources().getString(R.string.answer_10_question_1));
-                answers.add(c.getResources().getString(R.string.answer_10_question_2));
-                answers.add(c.getResources().getString(R.string.answer_10_question_3));
-                answers.add(c.getResources().getString(R.string.answer_10_question_4));
+                answers.add(new AnswerModel(10, c.getResources().getString(R.string.answer_10_question_1)));
+                answers.add(new AnswerModel(10, c.getResources().getString(R.string.answer_10_question_2)));
+                answers.add(new AnswerModel(10, c.getResources().getString(R.string.answer_10_question_3)));
+                answers.add(new AnswerModel(10, c.getResources().getString(R.string.answer_10_question_4)));
                 break;
         }
 
