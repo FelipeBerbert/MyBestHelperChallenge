8b328d653ec5a5e745f6ae9c341abf2983520921
angeloorrico <a.orrico@gmail.com>
2016-05-22 19:21:24 -0300

    Result activity.

--- app/src/main/java/vanhackathon2016/mybesthelperchallenge/gui/WizardActivity.java
+++ app/src/main/java/vanhackathon2016/mybesthelperchallenge/gui/WizardActivity.java
@@ -82,8 +82,7 @@ public class WizardActivity extends AppCompatActivity {
     }
 
     private void goToQuestionary() {
-        //Intent intent = new Intent(this, WelcomeActivity_.class);
-        Intent intent = new Intent(this, ResultActivity_.class);
+        Intent intent = new Intent(this, WelcomeActivity_.class);
         startActivity(intent);
     }
 
